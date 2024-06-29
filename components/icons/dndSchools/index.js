8a47a4fa74b0c49419881209fb8ebf0b9dfbf072
Miguel Arias <miguelariasgarau@gmail.com>
2024-06-29 10:42:17 +0200

    add svg icons for schools of magic

---
+++ components/icons/dndSchools/index.js
@@ -0,0 +1,19 @@
+import AbjurationIcon from './AbjurationIcon'
+import ConjurationIcon from './ConjurationIcon'
+import DivinationIcon from './DivinationIcon'
+import EnchantmentIcon from './EnchantmentIcon'
+import EvocationIcon from './EvocationIcon'
+import IllusionIcon from './IllusionIcon'
+import NecromancyIcon from './NecromancyIcon'
+import TransmutationIcon from './TransmutationIcon'
+
+export {
+    AbjurationIcon,
+    ConjurationIcon,
+    DivinationIcon,
+    EnchantmentIcon,
+    EvocationIcon,
+    IllusionIcon,
+    NecromancyIcon,
+    TransmutationIcon,
+}
