ede35f2d18bfe7da23d917e6c60777d986078d4c
Miguel Arias <miguelariasgarau@gmail.com>
2024-07-03 18:54:51 +0200

    add filter icon to start v2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { CoffeeIcon, GitHubIcon, MoonIcon, SunIcon } from "../components/icons"
+import { CoffeeIcon, GitHubIcon, MoonIcon, SunIcon, FilterIcon } from "../components/icons"
 import Class from "@/components/dndClasses/Class"
 
 export default function Home() {
@@ -59,6 +59,9 @@ export default function Home() {
 
             <nav className={'flex lg:justify-between px-5 py-2 box-border gap-3 overflow-y-auto border-b dark:border-neutral-700 border-neutral-200 items-center'}>
                 {/* <a>Create</a> */}
+                {/* <button onClick={}>
+                    <FilterIcon isDarkMode={isDarkMode} />
+                </button> */}
                 {dndClasses.map((cls) => (
                     <div style={{ backgroundColor: dndClass === cls ? (isDarkMode ? '#383838' : '#d5d5d5') : '', color: dndClass === cls ? (isDarkMode ? 'white' : '#383838') : '', padding: dndClass === cls ? '0.4rem' : '', borderRadius: dndClass === cls ? '0.375rem' : '' }} key={cls} onClick={() => handleDndClassClick(cls)}>{cls}</div>
                 ))}
--- components/icons/index.js
+++ components/icons/index.js
@@ -2,10 +2,12 @@ import GitHubIcon from './GithubIcon'
 import MoonIcon from './MoonIcon'
 import SunIcon from './SunIcon'
 import CoffeeIcon from './CoffeeIcon'
+import FilterIcon from './FilterIcon'
 
 export {
     GitHubIcon,
     MoonIcon,
     SunIcon,
     CoffeeIcon,
+    FilterIcon,
 }
